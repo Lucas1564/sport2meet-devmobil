881b7f153c8ae3fd0f14b5674cb4b9f33d4edd4e
Lucas1564 <lucascuennet@gmail.com>
2022-12-05 11:35:37 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sport2meet-devmobil',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
