0aaa0f85bfe15f9f66e2bd64b3be4e0bb902c0f9
AlexiaLeger <alexia.leger@heig-vd.ch>
2023-01-23 09:48:28 +0100

    initialise page actiité et chat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./auth/login/login.module').then(m => m.LoginPageModule)
-  }
+  },
 
 ];
 @NgModule({
--- src/app/layout/activities-list/activities-list-routing.module.ts
+++ src/app/layout/activities-list/activities-list-routing.module.ts
@@ -7,7 +7,11 @@ const routes: Routes = [
   {
     path: '',
     component: ActivitiesListPage
+  },  {
+    path: 'activity',
+    loadChildren: () => import('./activity/activity.module').then( m => m.ActivityPageModule)
   }
+
 ];
 
 @NgModule({
