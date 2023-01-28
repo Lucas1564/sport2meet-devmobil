eab7574c51e1cbae737c251e743a97b4ee87a603
Lucas1564 <lucascuennet@gmail.com>
2023-01-28 23:27:06 +0100

    Add css to app

--- src/app/layout/activities-list/activity/activity.module.ts
+++ src/app/layout/activities-list/activity/activity.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-
 import { ActivityPageRoutingModule } from './activity-routing.module';
-
 import { ActivityPage } from './activity.page';
 
 @NgModule({
