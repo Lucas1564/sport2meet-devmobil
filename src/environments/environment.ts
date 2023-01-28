5f3ee5cc00fffec0e165fd50174fc318d181e9c4
Lucas1564 <72100126+Lucas1564@users.noreply.github.com>
2023-01-28 23:35:43 +0100

    Delete environment.ts

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-// Copy this file to environment.ts and replace the values with your configuration
-export const environment = {
-  production: false,
-  apiUrl: "https://sport-2-meet.onrender.com",
-};
