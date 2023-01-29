4a7dd6d5b9d16d2d7c411395a00eac424b5c273b
Eloi Coquoz <eloi.coquoz@bluewin.ch>
2023-01-30 00:16:28 +0100

    Add capacitor/camera

--- src/app/layout/activities-list/activity/activity.page.ts
+++ src/app/layout/activities-list/activity/activity.page.ts
@@ -10,17 +10,24 @@ import { Storage } from "@ionic/storage";
 import { HttpClient } from '@angular/common/http';
 import { AlertController } from '@ionic/angular';
 import { activityUser } from 'src/app/models/activityUser';
+import { EventEmitter, Output } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
+
+import { ActionSheetController } from '@ionic/angular';
 @Component({
   selector: 'app-activity',
   templateUrl: './activity.page.html',
   styleUrls: ['./activity.page.scss'],
 })
 export class ActivityPage implements OnInit {
+  @Output() onImageUploaded = new EventEmitter();
+
 
   token!: string;
 
 
-  constructor(private alertController: AlertController,private router: Router,private activityService: ActivityService,private storage: Storage, private http: HttpClient) { }
+  constructor(private alertController: AlertController,private router: Router,private activityService: ActivityService,private storage: Storage, private http: HttpClient,private actionSheetController: ActionSheetController) { }
 
   async ngOnInit() {
   }
@@ -100,7 +107,52 @@ export class ActivityPage implements OnInit {
   onFileSelected(event: any) {
   }
 
-  uploadFile() {
+  async chooseTypePicture() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Select Image source',
+      buttons: [
+        {
+          text: 'Load from Library',
+          handler: async () => {
+            await this.takePicture(CameraSource.Photos);
+          },
+        },
+        {
+          text: 'Use Camera',
+          handler: () => {
+            this.takePicture(CameraSource.Camera);
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+      ],
+    });
+    await actionSheet.present();
+  }
+
+  async takePicture(source: CameraSource) {
+    try {
+      const permissions = await Camera.requestPermissions();
+      console.log(permissions);
+    } catch (e) {
+      console.log(e);
+    }
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Base64,
+      source,
+    });
+
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+
+    const data = image.base64String;
+    this.onImageUploaded.emit(data);
   }
 
   leaveActivity() {
